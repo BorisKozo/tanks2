b8f9a08340ef4fd8da942c8b93729bb07217976e
Boris Kozorovitzky <majorzbzzn@gmail.com>
2013-12-14 14:15:56 +0200

    Adding M1A

---
+++ assets/data/tanks/m1a_hull.js
@@ -0,0 +1,18 @@
+﻿define({
+  image: {
+    id: 'assets/sprites/m1a_hull.png',
+    path: 'assets/sprites/m1a_hull.png',
+    anchorX: 21,
+    anchorY: 9,
+    width: 42,
+    height: 20,
+    centerX: 21,
+    centerY: 10
+  },
+  spec: {
+    rotation: 0.003,
+    rotationDispersion: 0.1,
+    movementDispersion: 0.1,
+    speed: 0.2
+  }
+});
---
+++ assets/data/tanks/m1a_turret.js
@@ -0,0 +1,36 @@
+﻿define({
+  image: {
+    id: 'assets/sprites/m1a_turret.png',
+    path: 'assets/sprites/m1a_turret.png',
+    anchorX: 16,
+    anchorY: 9,
+    width: 47,
+    height: 19,
+    barrelEndX: 46,
+    barrelEndY: 9
+  },
+  sound: {
+    gunFire: {
+      path: 'assets/sounds/tank_firing_1.ogg',
+      id: 'assets/sounds/tank_firing_1.ogg'
+    }
+  },
+  spec: {
+    rotation: 0.005,
+    rotationDispersion: 0.2,
+    dispersion:{
+      min: 7, //This is degrees
+      max:19
+    },
+    aimingSpeed: 0.03,
+    reloadTime: 4000,
+    penetration:{
+      max: 150,
+      min: 110
+    },
+    damage: {
+      max: 120,
+      min: 90
+    }
+  }
+});
